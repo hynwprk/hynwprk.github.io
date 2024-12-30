db5376f8cb42c23e5f095ca19f25301c89de2399
hynwprk <hynwprk@gmail.com>
2024-12-30 06:22:13 +0000

    Deploying to gh-pages from @ hynwprk/hynwprk.github.io@7e7737e61dec3d772a1fdee8e8716b487ba02ec5 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -442,7 +442,12 @@ ninja.data = [{
             },},{id: "news-a-simple-inline-announcement-with-markdown-emoji-sparkles-smile",
           title: 'A simple inline announcement with Markdown emoji! :sparkles: :smile:',
           description: "",
-          section: "News",},{id: "projects-project-1",
+          section: "News",},{id: "projects-project-10",
+          title: 'project 10',
+          description: "another project with an image 🎉",
+          section: "Projects",handler: () => {
+              window.location.href = "/projects/10_project/";
+            },},{id: "projects-project-1",
           title: 'project 1',
           description: "with background image",
           section: "Projects",handler: () => {
